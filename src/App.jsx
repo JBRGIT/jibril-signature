50d421a5bceeddf6a566cde7ff66ac816116872d
Jibril RADI <jradi@insy2s.fr>
2025-09-22 14:53:42 +0200

    save

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,27 @@ import { useRef } from "react";
 import SignatureCanvas from "react-signature-canvas";
 function App() {
   const sigCanvas = useRef(null);
+
   const clear = () => {
     sigCanvas.current.clear();
   };
+
+  const save = () => {
+    const dataURL = sigCanvas.current.getCanvas().toDataURL("image/png");
+    const link = document.createElement("a");
+    link.href = dataURL;
+    link.download = "signature.png";
+    link.click();
+  };
+
   return (
     <div>
       <SignatureCanvas ref={sigCanvas} />
-      <button onClick={() => clear()} type="reset">
+      <button onClick={() => clear()} type="button">
         reset
+      </button>
+      <button onClick={() => save()} type="button">
+        save
       </button>
     </div>
   );
